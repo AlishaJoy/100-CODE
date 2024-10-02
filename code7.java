3808fe1f48958a19b8272ef34054468a51357009
Alisha Joy <130027011+AlishaJoy@users.noreply.github.com>
2024-10-02 22:10:02 +0530

    LEAP YEAR

---
+++ code7.java
@@ -0,0 +1,17 @@
+// LEAP YEAR 
+public class Main
+ {
+   public static void main (String[]args)
+   {
+
+     int year = 2020;
+
+     if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+       System.out.println (year + " is a Leap Year");
+
+     //not leap year
+     else
+         System.out.println (year + " is not a Leap Year");
+
+   }
+ }
